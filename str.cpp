2d91e453e1fdfa22bf314510b558c2f7eeed6fe3
SorerBOT <alonfiller@gmail.com>
2022-05-11 10:51:48 +0300

    (Patch) implemented Str != operator

--- str.cpp
+++ str.cpp
@@ -9,3 +9,4 @@ Str::Str(const char *str) { delete this->m_str; this->m_str = strdup(str); }
 Str::~Str() { delete this->m_str; }
 
 bool Str::operator==(const Str &other) const { return strcmp(this->m_str, other.getStr()) == 0; }
+bool Str::operator!=(const Str &other) const { return strcmp(this->m_str, other.getStr()) != 0; }
