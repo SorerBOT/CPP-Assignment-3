1754572ef81a30f0398d3b5bcd51e8cdbd44c06b
SorerBOT <alonfiller@gmail.com>
2022-05-11 10:47:54 +0300

    (Patch) implemented Str == operator

--- str.cpp
+++ str.cpp
@@ -7,3 +7,5 @@
 Str::Str() { this->m_str = strdup("none"); }
 Str::Str(const char *str) { delete this->m_str; this->m_str = strdup(str); }
 Str::~Str() { delete this->m_str; }
+
+bool Str::operator==(const Str &other) const { return strcmp(this->m_str, other.getStr()) == 0; }
