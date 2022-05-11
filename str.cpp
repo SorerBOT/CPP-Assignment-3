a5a70510527aa04f333c1ea6a9a60afdd8664c1c
SorerBOT <alonfiller@gmail.com>
2022-05-11 10:55:36 +0300

    (Patch) implemented Str < operator

--- str.cpp
+++ str.cpp
@@ -11,3 +11,4 @@ Str::~Str() { delete this->m_str; }
 bool Str::operator==(const Str &other) const { return strcmp(this->m_str, other.getStr()) == 0; }
 bool Str::operator!=(const Str &other) const { return strcmp(this->m_str, other.getStr()) != 0; }
 bool Str::operator>(const Str &other) const { return strcmp(this->m_str, other.getStr()) > 0; }
+bool Str::operator<(const Str &other) const { return strcmp(this->m_str, other.getStr()) < 0; }
